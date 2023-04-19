e1d3a82a1b0225c4a186bfbadd3f9e07bb765a62
Jason Tavera <taverajason7@gmail.com>
2023-04-18 22:12:09 -0500

    made changes to javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
+let dayOne = document.querySelector("#day-one")
 
 
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=953f193781e1d680d421d184dd1910c7")
-    .then(response => response.json())
-    .then(citiesFound => {
-        let firstCity = citiesFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
-
-        return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=953f193781e1d680d421d184dd1910c7`)
-    })
-
+.then(response => response.json())
+.then(citiesFound => {
+    let firstCity = citiesFound[0];
+    console.log(firstCity.lat);
+    console.log(firstCity.lon);
     
-    .then(response => response.json())
-    .then(data => {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=953f193781e1d680d421d184dd1910c7&units=imperial`)
+})
 
-        console.log(data);
+
+.then(response => response.json())
+.then(data => {
+    let dayOneWeather = {
+        temp: data.list[0].main.temp,
+        wind: data.list[0].wind.speed,
+        humidity: data.list[0].main.humidity,
+    }
+    console.log(data);
+    console.log(dayOneWeather)
+    dayOne.innerHTML = `<h5 class="card-title">Temp: ${dayOneWeather.temp}</h5>
+    <h5 class="card-title">Wind: ${dayOneWeather.wind}</h5>
+    <h5 class="card-title">Humidity: ${dayOneWeather.humidity}</h5>`
+
     })
